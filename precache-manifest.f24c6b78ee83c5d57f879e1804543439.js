be997da82ff9d0370fb46c04c773ffd746d9e4ff
TommasoVitali <tommyvitaly@gmail.com>
2020-10-12 12:10:34 -0400

    Updates

---
+++ precache-manifest.f24c6b78ee83c5d57f879e1804543439.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad66825fd13d9bb2d763ab8ec6772707",
+    "url": "/Painter-React-Website/index.html"
+  },
+  {
+    "revision": "9cfbd17b004b7e4d24b3",
+    "url": "/Painter-React-Website/static/css/main.7e16c1dd.chunk.css"
+  },
+  {
+    "revision": "93cd38d472939d67ed27",
+    "url": "/Painter-React-Website/static/js/2.55023c5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Painter-React-Website/static/js/2.55023c5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cfbd17b004b7e4d24b3",
+    "url": "/Painter-React-Website/static/js/main.351895cc.chunk.js"
+  },
+  {
+    "revision": "fd78a9ef56bee3f671c0",
+    "url": "/Painter-React-Website/static/js/runtime-main.7e693643.js"
+  },
+  {
+    "revision": "cd1914fff6788b931ed3628d0de1aef3",
+    "url": "/Painter-React-Website/static/media/Facebook2.cd1914ff.jpg"
+  },
+  {
+    "revision": "7ad276e36ff6464c18b235da74f810fb",
+    "url": "/Painter-React-Website/static/media/Instagram.7ad276e3.jpg"
+  },
+  {
+    "revision": "a58de98224f821c982ffb90c7a2ce3f4",
+    "url": "/Painter-React-Website/static/media/Tv2.a58de982.png"
+  },
+  {
+    "revision": "6a7b5468c44d162a6820823a803bd396",
+    "url": "/Painter-React-Website/static/media/amadeus.6a7b5468.jpg"
+  },
+  {
+    "revision": "19cce2eb5e983bd67b731cee87cd5fe0",
+    "url": "/Painter-React-Website/static/media/amadeus2.19cce2eb.jpg"
+  },
+  {
+    "revision": "26ed2dff270857718d6e865fc462a911",
+    "url": "/Painter-React-Website/static/media/amadeus2small.26ed2dff.jpg"
+  },
+  {
+    "revision": "6e970201c56f4209a13ae6e11cc1ee18",
+    "url": "/Painter-React-Website/static/media/amadeussmall.6e970201.jpg"
+  },
+  {
+    "revision": "79b507436a1cbfaf8ef6c7d71655abdb",
+    "url": "/Painter-React-Website/static/media/blackgold.79b50743.jpg"
+  },
+  {
+    "revision": "5339b2e78988b8e620805e4b7b773303",
+    "url": "/Painter-React-Website/static/media/blackgold2.5339b2e7.jpg"
+  },
+  {
+    "revision": "421db598759385c23782b0fd1722e68b",
+    "url": "/Painter-React-Website/static/media/blackgold2small.421db598.jpg"
+  },
+  {
+    "revision": "64d23fb5d32de2b0be2b7d8113c7233e",
+    "url": "/Painter-React-Website/static/media/blackgold4.64d23fb5.jpg"
+  },
+  {
+    "revision": "890ea20785b0d4e4fd8a07022d033fde",
+    "url": "/Painter-React-Website/static/media/blackgold4small.890ea207.jpg"
+  },
+  {
+    "revision": "5a2742891e033131c4d41ffc435037ee",
+    "url": "/Painter-React-Website/static/media/bushido.5a274289.jpg"
+  },
+  {
+    "revision": "d93384a58867a3f5c8bc7c0cc2f5b56e",
+    "url": "/Painter-React-Website/static/media/bushido1.d93384a5.jpg"
+  },
+  {
+    "revision": "079b80bc1d3c770d218ee0ecd32a7241",
+    "url": "/Painter-React-Website/static/media/bushido2.079b80bc.jpg"
+  },
+  {
+    "revision": "20a7cded98c1e48d389289fbdee23ac6",
+    "url": "/Painter-React-Website/static/media/bushido3.20a7cded.jpg"
+  },
+  {
+    "revision": "1c37e64e91d6d12cee74090a841c1de2",
+    "url": "/Painter-React-Website/static/media/bushido4.1c37e64e.jpg"
+  },
+  {
+    "revision": "7a17ab042d81db0e8ef03631650b161b",
+    "url": "/Painter-React-Website/static/media/bushidosmall2.7a17ab04.jpg"
+  },
+  {
+    "revision": "512c587db7909b851ba1504d897df944",
+    "url": "/Painter-React-Website/static/media/bushidosmall3.512c587d.jpg"
+  },
+  {
+    "revision": "8ad9a46e42c47668037cbfd115581f48",
+    "url": "/Painter-React-Website/static/media/canvas.8ad9a46e.jpg"
+  },
+  {
+    "revision": "4ffbfbb15f554bd93c2f2d9fd8b39477",
+    "url": "/Painter-React-Website/static/media/clouds.4ffbfbb1.jpg"
+  },
+  {
+    "revision": "f77bb7ba5aa159adb957151f830f9497",
+    "url": "/Painter-React-Website/static/media/clouds2.f77bb7ba.jpg"
+  },
+  {
+    "revision": "de9fc4950498ef8448b77f432d97c36c",
+    "url": "/Painter-React-Website/static/media/clouds2small.de9fc495.jpg"
+  },
+  {
+    "revision": "99bc6ff02f040a0b60fe99f9bb192c90",
+    "url": "/Painter-React-Website/static/media/clouds3.99bc6ff0.jpg"
+  },
+  {
+    "revision": "323ff569bd7206fce1d730e67145ad6e",
+    "url": "/Painter-React-Website/static/media/clouds3small.323ff569.jpg"
+  },
+  {
+    "revision": "ae1d2978eb1b2ae487854f6ccfc06eea",
+    "url": "/Painter-React-Website/static/media/cloudssmall.ae1d2978.jpg"
+  },
+  {
+    "revision": "88063eca7e112fe0dac7fdc0828c778c",
+    "url": "/Painter-React-Website/static/media/deepblue.88063eca.jpg"
+  },
+  {
+    "revision": "17634c007dc0304af09bc402fefbb2c8",
+    "url": "/Painter-React-Website/static/media/deepblue2.17634c00.jpg"
+  },
+  {
+    "revision": "d788cc3a18070f070c8c5ba9ae228bc7",
+    "url": "/Painter-React-Website/static/media/deepblue3.d788cc3a.jpg"
+  },
+  {
+    "revision": "0f981d485ff498370b11c4d1829f4ad5",
+    "url": "/Painter-React-Website/static/media/deepblue4.0f981d48.jpg"
+  },
+  {
+    "revision": "fec13b0736d265344a8c7e664a3dfc2c",
+    "url": "/Painter-React-Website/static/media/deepbluesmall.fec13b07.jpg"
+  },
+  {
+    "revision": "b437cdebb3e9d4cc803bdbed97a72681",
+    "url": "/Painter-React-Website/static/media/deepbluesmall4.b437cdeb.jpg"
+  },
+  {
+    "revision": "2f01b4e61aea9fdc5bd59f6b63a395fa",
+    "url": "/Painter-React-Website/static/media/dividersmall2.2f01b4e6.png"
+  },
+  {
+    "revision": "8f28488097b7e0cf51695da5a4a893a9",
+    "url": "/Painter-React-Website/static/media/empire.8f284880.jpg"
+  },
+  {
+    "revision": "d8f65962996b335052b5d6fbf72b4a35",
+    "url": "/Painter-React-Website/static/media/immaginazione.d8f65962.jpg"
+  },
+  {
+    "revision": "738ca02a7d64d80c04701b96a304d90d",
+    "url": "/Painter-React-Website/static/media/immaginazione2.738ca02a.jpg"
+  },
+  {
+    "revision": "103163f6584e55f7dd021fb4b99fc228",
+    "url": "/Painter-React-Website/static/media/immaginazione2small.103163f6.jpg"
+  },
+  {
+    "revision": "dd7f9f53110c8460a36642a3227a2732",
+    "url": "/Painter-React-Website/static/media/immaginazione3.dd7f9f53.jpg"
+  },
+  {
+    "revision": "fe0edf8c23d4fd0dc82ae496a0322918",
+    "url": "/Painter-React-Website/static/media/immaginazione3small.fe0edf8c.jpg"
+  },
+  {
+    "revision": "7d00568a8d6eded7c22cb4167ccadbf7",
+    "url": "/Painter-React-Website/static/media/immaginazionesmall.7d00568a.jpg"
+  },
+  {
+    "revision": "059a0e8d676890e2ec4c1dd27a1f329b",
+    "url": "/Painter-React-Website/static/media/mediceo.059a0e8d.jpg"
+  },
+  {
+    "revision": "226e778ea74d5168cee4828bba4f6b9f",
+    "url": "/Painter-React-Website/static/media/mediceo2.226e778e.jpg"
+  },
+  {
+    "revision": "54c0339a37656ff0bf6e513b510f2149",
+    "url": "/Painter-React-Website/static/media/mediceo2small.54c0339a.jpg"
+  },
+  {
+    "revision": "6be356cfc642ce917da4a6b5e80423d1",
+    "url": "/Painter-React-Website/static/media/mediceosmall.6be356cf.jpg"
+  },
+  {
+    "revision": "84dd3a9ce1b859d504989a557a912f5f",
+    "url": "/Painter-React-Website/static/media/ra.84dd3a9c.jpg"
+  },
+  {
+    "revision": "a63990e7731b876d0a62d00441ab08f5",
+    "url": "/Painter-React-Website/static/media/ra2.a63990e7.jpg"
+  },
+  {
+    "revision": "9b1a9ba6a4baff406f229001cfd4961f",
+    "url": "/Painter-React-Website/static/media/ra2small.9b1a9ba6.jpg"
+  },
+  {
+    "revision": "3b59c2e7c69c09b465708bd86b57281c",
+    "url": "/Painter-React-Website/static/media/ra3.3b59c2e7.jpg"
+  },
+  {
+    "revision": "650b43b5dc2821b860a9a7e923661f00",
+    "url": "/Painter-React-Website/static/media/ra3small.650b43b5.jpg"
+  },
+  {
+    "revision": "b6aefd7a808e7dfd68208e43f3bb0dab",
+    "url": "/Painter-React-Website/static/media/reddeath.b6aefd7a.jpg"
+  },
+  {
+    "revision": "646f07b31235729365a702308740d381",
+    "url": "/Painter-React-Website/static/media/reddeath2.646f07b3.jpg"
+  },
+  {
+    "revision": "8aafb31150b55dcdf03cdc39aa790129",
+    "url": "/Painter-React-Website/static/media/reddeath2small.8aafb311.jpg"
+  },
+  {
+    "revision": "4603c93d2b589e55df0c0962bc9c4622",
+    "url": "/Painter-React-Website/static/media/reddeath3.4603c93d.jpg"
+  },
+  {
+    "revision": "f4d2cc4bfd2f2f1e35e4fd1aa802e5ea",
+    "url": "/Painter-React-Website/static/media/reddeath3small.f4d2cc4b.jpg"
+  },
+  {
+    "revision": "d9b303e9396ea9299d6bbd7a0fd50ba0",
+    "url": "/Painter-React-Website/static/media/sons.d9b303e9.jpg"
+  },
+  {
+    "revision": "a5cee1684aa798ef4656151bbabdae59",
+    "url": "/Painter-React-Website/static/media/sonsofodin.a5cee168.jpg"
+  },
+  {
+    "revision": "b63b805513b36f77ddedadf12f24bf00",
+    "url": "/Painter-React-Website/static/media/sonsofodinsmall.b63b8055.jpg"
+  },
+  {
+    "revision": "57b20fe3b55853a505a96e2e075cb431",
+    "url": "/Painter-React-Website/static/media/sonssmall.57b20fe3.jpg"
+  },
+  {
+    "revision": "3da015c0029d2d975117d523ab9319c8",
+    "url": "/Painter-React-Website/static/media/stargate.3da015c0.jpg"
+  },
+  {
+    "revision": "54fc234f64fc8672e8622deb7f039c9f",
+    "url": "/Painter-React-Website/static/media/stargatebig.54fc234f.jpg"
+  },
+  {
+    "revision": "b6cde8d5800551b6ba1db20fdfca7236",
+    "url": "/Painter-React-Website/static/media/stargatesmall1.b6cde8d5.jpg"
+  },
+  {
+    "revision": "0f591a5930caebfc8a2d14e3cf3406ce",
+    "url": "/Painter-React-Website/static/media/stargatesmall2.0f591a59.jpg"
+  },
+  {
+    "revision": "212a96ae1a748e7de17a7d75d874b840",
+    "url": "/Painter-React-Website/static/media/stargatewall.212a96ae.jpg"
+  },
+  {
+    "revision": "356f2bcf2d05a5da7bad75609901a105",
+    "url": "/Painter-React-Website/static/media/tommyjpg.356f2bcf.jpg"
+  }
+]);
